7874d81ebe1a2127bd7da7ee1aa83adf581cb75b
Anwaar <anwaaralsyouf@gmail.com>
2023-02-07 23:47:53 +0300

    main files created

---
+++ shuffel-names/app.js
@@ -0,0 +1,13 @@
+const names = [
+    'Abdulla', 'Ahlam', 'Ahmed Hisham',
+    'Ahmed Al-Slaibi', 'Ali Lababidi', 'Ali Hadi',
+    'Ali Naser', 'Aref', 'Bader',
+    'Eman', 'Esraa', 'Fatima',
+    'Hajar', 'Hasan Alsowaidi', 'Hasan Marhoon',
+    'Hassan	Al Hashimi', 'Hassan Khushbakhat', 'Haya',
+    'Jehan', 'Madhawi', 'Mahmood',
+    'Maryam', 'Mohamed Alalwan', 'Mohammed Abdulrahim',
+    'Nadia', 'Noor', 'Noroz',
+    'Salman', 'Sayed', 'Yusuf', 'Zahid'
+]
+
