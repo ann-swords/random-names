4dbc17932428e5b625f9e534acec912bc401e75e
Anwaar <anwaaralsyouf@gmail.com>
2023-02-08 14:50:46 +0300

    random names function created

--- shuffel-names/app.js
+++ shuffel-names/app.js
@@ -11,3 +11,25 @@ const names = [
     'Salman', 'Sayed', 'Yusuf', 'Zahid'
 ]
 
+
+const randomBtn = document.querySelector('.randomBtn') 
+const student = document.querySelector('.student') 
+//Randomizing the names
+for (let i = 0; i < names.length; i++) {
+    const randomPosition = Math.floor((names.length - i) * Math.random())
+    const randomItem = names.splice(randomPosition, 1)
+    
+    names.push(...randomItem)
+}
+
+function getRandomName (){
+    if(names.length !== 0){
+        // student.innerText = names.pop() 
+        student.innerText = names.pop() 
+        console.log(names)
+        }
+    }
+
+randomBtn.addEventListener('click', getRandomName)
+
+
